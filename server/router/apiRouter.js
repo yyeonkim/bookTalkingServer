e2d8a3743684cdbbe734be8579c7bb709abf3f74
Yong-yeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2023-05-26 21:52:09 +0900

    chore: 시스템 메시지 삭제

--- server/router/apiRouter.js
+++ server/router/apiRouter.js
@@ -9,19 +9,9 @@ const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
-const systemMessage = {
-  role: "system",
-  content:
-    "You are a Korean elementary school friend. Your task is to generate story questions of 이상한 나라의 앨리스 to help the user to cultivate imagination. You have to ask shortly less than 60 words and say in Korean. For your first assignment, you are tasked with saying hello to the user named 연이 and asking a question.",
-};
-const remindSystemMessage = {
-  role: "system",
-  content:
-    "You are a Korean elementary school friend. Your task is to generate story questions of 이상한 나라의 앨리스 to help the user named 연이 to cultivate imagination. You have to ask shortly less than 60 words and generate one question at a time based on 연이's answer. Don't say hello and say in Korean.",
-};
 const chatSettings = {
   model: "gpt-3.5-turbo",
-  temperature: 0.3,
+  temperature: 0.5,
   top_p: 1,
   presence_penalty: 0,
   frequency_penalty: 0.5,
@@ -29,7 +19,7 @@ const chatSettings = {
 };
 const createSettings = {
   model: "gpt-3.5-turbo",
-  temperature: 0.3,
+  temperature: 0.8,
   top_p: 1,
   presence_penalty: 0,
   frequency_penalty: 0.5,
@@ -42,9 +32,9 @@ const keywordSettings = {
   frequency_penalty: 0.8,
 };
 
-router.get("/chat/start", async (req, res) => {
+router.post("/chat/start", async (req, res) => {
   const completion = await openai.createChatCompletion({
-    messages: [systemMessage],
+    messages: [req.body],
     ...chatSettings,
   });
 
@@ -53,7 +43,7 @@ router.get("/chat/start", async (req, res) => {
 
 router.post("/chat/send", async (req, res) => {
   const completion = await openai.createChatCompletion({
-    messages: [...req.body, remindSystemMessage],
+    messages: req.body,
     ...chatSettings,
   });
 
@@ -79,7 +69,7 @@ router.post("/chat/keyword", async (req, res) => {
   // 요약한 동화를 body로 보낸다.
   const completion = await openai.createChatCompletion({
     messages: [
-      ,
+      req.body,
       {
         role: "user",
         content: `${req.body}\n위의 이야기에서 다섯 개의 키워드를 영어로 알려줘. 아래처럼 리스트 형태로 알려줘.\nanswer: [word1, word2, word3, word4, word5]`,
