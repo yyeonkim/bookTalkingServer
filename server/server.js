9ef8bcbe63f18237e5a19d95ca9455ea85a8f598
Yong-yeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2023-05-30 18:56:10 +0900

    build: react 배포 파일 설정

--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
+
 const express = require("express");
 const app = express();
 const apiRouter = require("./router/apiRouter");
 
+app.use(express.static(path.join(__dirname, "../client/build")));
 app.use(express.json());
 app.use("/api", apiRouter);
 
-const port = 5000;
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+});
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
