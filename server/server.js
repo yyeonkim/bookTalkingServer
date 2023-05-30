449122cf2ed7ff6bd12f587b78fbb5c38b982755
Yong-yeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2023-05-30 20:02:33 +0900

    build: 파일 압축하기

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const path = require("path");
+const compression = require("compression");
 require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 
 const express = require("express");
 const app = express();
 const apiRouter = require("./router/apiRouter");
 
+app.use(compression());
 app.use(express.static(path.join(__dirname, "../client/build")));
 app.use(express.json());
 app.use("/api", apiRouter);
