be8362eedf7da09cb1115fa3dd2202e558ffe384
Yong-yeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2023-05-30 23:59:13 +0900

    chore: 리액트가 라우팅

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, "../client/build")));
 app.use(express.json());
 app.use("/api", apiRouter);
 
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
