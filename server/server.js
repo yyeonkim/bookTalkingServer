fe335054e81e96d308abe34bddd321f90e88b48c
Yong-yeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2023-05-30 23:05:23 +0900

    chore: 포트 번호 변경

--- server/server.js
+++ server/server.js
@@ -15,5 +15,5 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
-const port = process.env.PORT || 5000;
+const port = 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
