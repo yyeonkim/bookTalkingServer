435eadc35fb978c87ff89880809c3c721bb194d3
Yong-yeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2023-05-31 00:19:39 +0900

    chore: cors 설정

--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
 const path = require("path");
 const compression = require("compression");
+const cors = require("cors");
 require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 
 const express = require("express");
 const app = express();
 const apiRouter = require("./router/apiRouter");
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 app.use(compression());
 app.use(express.static(path.join(__dirname, "../client/build")));
 app.use(express.json());
